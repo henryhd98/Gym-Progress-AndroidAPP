d6f5fd85b177c72e6a6c47f5d9e57bf4be42193d
henryhd98 <durujih@ymail.com>
2019-03-06 23:23:19 +0000

    got the date input to work now i need to figure out how to store it into the database so i can track users date etc

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,8 +5,10 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -14,7 +16,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG ="MainActivity";
-    private TextView mDisplayDate;
+    private Button mDisplayDate;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
 
     Button enterBW;
@@ -30,16 +32,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         enterBW= (Button) findViewById(R.id.enterBW) ;
-        enterdate= (Button) findViewById(R.id.enterdate) ;
+
         View = (TextView) findViewById(R.id.dbcontent);
         dbView = (TextView) findViewById(R.id.dbcontent);
-        mDisplayDate = (TextView) findViewById(R.id.tvDate);
+        mDisplayDate = (Button) findViewById(R.id.tvDate);
         BPDB = new benchpressDetails(getApplicationContext());
         showFullDatabase();
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(android.view.View v) {
+            public void onClick(View view) {
                 Calendar cal = Calendar.getInstance();
 
 
@@ -47,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 int year =cal.get(Calendar.YEAR);
                 int month =cal.get(Calendar.MONTH);
                 int day =cal.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog dialog =new DatePickerDialog(MainActivity.this, android.R.style.Theme_Material_Dialog_MinWidth, mDateSetListener, year, month, day);
+                DatePickerDialog dialog =new DatePickerDialog(MainActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener, year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
 
@@ -55,6 +57,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month=month +1;
+                Log.d(TAG,"onDateSet: mm/dd/yyyy:" +month + "/" + day + "/" + year);
+                String date = month + "/" + day + "/" + year ;
+                mDisplayDate.setText(date);
+            }
+        };
     }
 
     private void showFullDatabase() {
